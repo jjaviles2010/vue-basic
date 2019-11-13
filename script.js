a4601b2d7800af869291226838f7cb33b5047f5b
FIAP <FIAP>
2019-11-12 22:26:54 -0300

    Implementando lista com directiva vue

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
+})
+
 var app  = new Vue({
     el: '#app',
     data: {
-        message: "Hello Vue!"
+        message: "Hello Vue!",
+        lista: [
+            {id: 0, text: "tomate"},
+            {id: 1, text: "laranja"},
+            {id: 2, text: "ovo"}
+        ]
     }
 });
